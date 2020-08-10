ea9a7748387ea6b736d86e1100c61bdd4364d129
LE <llacb47@users.noreply.github.com>
2020-08-10 15:13:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -77,21 +77,19 @@ class TikTok:
         device_id = ''.join(random.choice('0123456789') for _ in range(19))
 
         data = (
-            f'action_time={action_time}&item_id={self.video_id}&item_type=1&share_delta=1&stats_cha'
-            'nnel=copy'
+            f'aweme_type=0&item_id={self.video_id}&share_delta=1'
         )
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded',
-            'x-common-params-v2': 'version_code=16.6.5&app_name=musical_ly&channel=App%20Store&devi'
-                                  f'ce_id={device_id}&aid=1233&os_version=13.5.1&device_platform=ip'
-                                  'hone&device_type=iPhone10,5',
-            'User-Agent': 'TikTok 16.6.5 rv:166515 (iPhone; iOS 13.6; sv_SE) Cronet'
+            'x-common-params-v2': 'version_code=16.6.1&app_name=musical_ly&channel=App%20Store&devi'
+                                  f'ce_id={device_id}&aid=1233&os_version=13.5&device_platform=ip'
+                                  'hone&device_type=iPhone8,1',
+            'User-Agent': 'TikTok 16.6.1 rv:166103 (iPhone; iOS 13.5; es_US) Cronet'
         }
 
         try:
             response = requests.post(
-                'https://api16-core-c-useast1a.tiktokv.com/aweme/v1/aweme/stats/?ac=WIFI&op_region='
-                'SE&app_skin=white&', data=data, headers=headers
+                'https://api16-core-c-useast1a.tiktokv.com/aweme/v1/aweme/stats/?ac=WIFI&op_region=US&app_skin=white&', data=data, headers=headers
             )
         except Exception as e:
             print(f'Error: {e}')
